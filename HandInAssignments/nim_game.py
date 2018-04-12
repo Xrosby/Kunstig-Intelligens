051ebc4c7bc36f42aa1f3ec43222010f0f7f21f2
Xrosby <mads.sfg@gmail.com>
2018-04-12 13:11:44 +0200

    update

--- HandInAssignments/nim_game.py
+++ HandInAssignments/nim_game.py
@@ -23,11 +23,34 @@ def minmax_decision(state):
 
 
 def is_terminal(state):
-    return same_elements(state)
+
+    distinct_element = False
 
 
-def same_elements(iterator):
-    return len(set(iterator)) <= 1
+    current_element = int(state[0])
+    skipped_elements = 0
+    skipped_elementIndex = 0
+
+    for i in range(1, len(state)):
+        if state[i] == current_element:
+            current_element = state[i]
+        else:
+            skipped_elements += 1
+            skipped_elementIndex = i
+
+
+    state.remove(skipped_elementIndex)
+
+
+
+    if skipped_elements == 1 and state[0] == 1:
+        distinct_element = True
+
+
+
+    return distinct_element
+
+
 
 
 def utility_of(state):
@@ -47,7 +70,7 @@ def successors_of(state):
     """
 
     for pile in state:
-        for i in range(0, len(pile)):
+        for i in range(0, pile):
             print(i)
             if i != pile and i != pile/2:
                 list_of_successor_states.append((i, pile-i))
@@ -60,15 +83,22 @@ def display(state):
 
 
 def main():
-    board = [15]
+    board = [15, 1]
     while not is_terminal(board):
+        board[minmax_decision(board)] = 2
+    if not is_terminal(board):
+        display(board)
+        board[(int(input("Your nove? ")))] = 2
+
+
+    """    while not is_terminal(board):
         board[minmax_decision(board)] = 'X'
         if not is_terminal(board):
             display(board)
             board[int(input('Your move? '))] = 'O'
     display(board)
 
-
+    """
 def argmax(iterable, func):
     return max(iterable, key=func)
 
